f28741df0f73377e91e6b0c2946a04b891d5feb3
JayBagal <jayrambagal00@gmail.com>
2024-01-20 19:00:50 +0530

    Preload script

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-import {app, BrowserWindow} from "electron";
+const {app, BrowserWindow} = require("electron/main");
+const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
-  win.loadFile("index.html");
+  win.loadURL("https://jayrambagal.vercel.app/");
+
+  const contents = win.webContents;
+  console.log(contents);
 };
 
 app.whenReady().then(() => {
   createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
